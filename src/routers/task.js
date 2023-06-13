b2fa7c13e8d514a783a251365258468d1aa027c1
PrakharPandeyyy <8.prakharpandey@gmail.com>
2023-06-13 21:55:17 +0530

    added queries

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,11 +19,34 @@ router.post("/tasks", auth ,async (req,res)=>{
     }
 })
 
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=10
+// GET /tasks?sortBy=createdAt:desc
+
 router.get("/tasks",auth , async (req,res)=>{
     try {
-        // const task=await Task.find({owner :req.user._id})
+        const match ={}
+        const sort = {}
+
+            if(req.query.completed){
+                match.completed = req.query.completed === 'true'
+            }
+            if(req.query.sortBy){
+                const parts = req.query.sortBy.split(':')
+                sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+
+            }
         
-        const user = await req.user.populate('tasks')
+        const user = await req.user.populate({
+            path : 'tasks',
+            match,
+            options : {
+                limit :parseInt(req.query.limit),
+                skip:parseInt(req.query.skip),
+                sort 
+            }
+            
+        }) 
         res.status(201).send({status:'success',data:user.tasks})
     }
     catch(error){
